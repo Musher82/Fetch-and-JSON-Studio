a92df82d0cf36f6164a2b8dd29b4c91cf0738b1e
Micah <mrusher@gmail.com>
2021-04-02 01:28:53 -0500

    Added event listener and fetch

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+  let json = [];
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+    response.json().then(function(json){
+   
+
+    })
+  })
+})
